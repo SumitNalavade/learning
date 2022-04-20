0cf3c82d46a9fbcd2e8e7c18ca77fa91ddb83eb1
SumitNalavade <vs.nalavade2003@gmail.com>
2022-04-20 02:32:55 -0500

    Add press enter to create new todo

--- client/src/Components/NewTodoInput/NewTodoInput.tsx
+++ client/src/Components/NewTodoInput/NewTodoInput.tsx
@@ -12,15 +12,17 @@ interface Props {
 const NewTodoInput: React.FC<Props> = ({ createNewTodoMutation }) => {
     const [inputValue, setInputValue] = useState<string>("");
 
-    const handleCreateTodo = () => {
-        createNewTodoMutation.mutate({ name: inputValue, complete: false })
-    };
+    const handleSubmit = (evt: React.KeyboardEvent<HTMLInputElement>) => {
+        if(evt.key === "Enter") {
+            createNewTodoMutation.mutate({ name: inputValue, complete: false })
+        }
+    }
 
     return (
         <InputGroup size="md" mb={12} style={{display: "flex", alignItems: "center"}} >
             <InputLeftAddon children='I want to...' />
-            <Input value={inputValue} onChange={(evt) => setInputValue(evt.target.value)} placeholder='To-Do title' />
-            <IconButton onClick={handleCreateTodo} mx={2} size='sm' icon={<CheckIcon color="green" />} aria-label={""} />
+            <Input value={inputValue} onKeyDown={handleSubmit} onChange={(evt) => setInputValue(evt.target.value)} placeholder='To-Do title' />
+            <IconButton onClick={() => createNewTodoMutation.mutate({ name: inputValue, complete: false })} mx={2} size='sm' icon={<CheckIcon color="green" />} aria-label={""} />
         </InputGroup>
     );
 };
